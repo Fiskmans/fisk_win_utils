01ccb99f464481fd1e36e6ec67c193db23ac714c
Fiskmans - GLaDOS <mans@zingo.org>
2024-08-19 17:47:47 +0200

    add imgui_tree

--- example/main.cpp
+++ example/main.cpp
@@ -4,6 +4,18 @@
 #include "win_utils/ImguiHelper.h"
 
 #include "imgui.h"
+#include "imgui_tree.h"
+
+struct TreeLike
+{
+	TreeLike(std::string aName)
+	{
+		myName = aName;
+	}
+
+	std::string myName;
+	std::vector<TreeLike> myNodes;
+};
 
 int main()
 {
@@ -14,6 +26,22 @@ int main()
 	fisk::tools::V4f clearColor = { 0.5f, 0.5f, 0.5f, 1.f };
 	bool demoWindowEnabled = false;
 	bool vSync = true;
+	float rounding = 0.f;
+	float indentDepth = 15.f;
+	float thickness = 1.f;
+	fisk::tools::V4f treeColor = { 0.5f, 0.5f, 0.5f, 1.f };
+
+	TreeLike tree("Root");
+
+	tree.myNodes.emplace_back("Node 1");
+	tree.myNodes[0].myNodes.emplace_back("Leaf 1");
+	tree.myNodes[0].myNodes.emplace_back("Leaf 2");
+	
+	tree.myNodes.emplace_back("Node 2");
+	tree.myNodes[1].myNodes.emplace_back("Leaf 3");
+
+	tree.myNodes.emplace_back("Node 3");
+	tree.myNodes[2].myNodes.emplace_back("Leaf 4\nmultiline\nadventures");
 
 	fisk::tools::EventReg demoWindowHandle = imgui.DrawImgui.Register([&]()
 	{
@@ -22,6 +50,28 @@ int main()
 		ImGui::Checkbox("Imgui demo", &demoWindowEnabled);
 		ImGui::ColorEdit4("Clear color", clearColor.Raw());
 		ImGui::Checkbox("V sync", &vSync);
+
+		if (ImGui::CollapsingHeader("Tree"))
+		{
+			ImGui::DragFloat("Rounding", &rounding);
+			ImGui::DragFloat("Indent", &indentDepth);
+			ImGui::DragFloat("Thickness", &thickness);
+			ImGui::ColorEdit4("Color", treeColor.Raw());
+
+			imgui_extensions::Tree<TreeLike>(tree, 
+				[](TreeLike& aNode)
+				{
+					ImGui::Text(aNode.myName.c_str());
+				},
+				[](TreeLike& aNode) -> auto&
+				{
+					return aNode.myNodes;
+				},
+				ImColor(treeColor[0], treeColor[1], treeColor[2], treeColor[3]),
+				indentDepth,
+				rounding,
+				thickness);
+		}
 
 		ImGui::End();
 
---
+++ imgui/imgui_tree.h
@@ -0,0 +1,116 @@
+
+#include "imgui.h"
+
+#include <concepts>
+#include <vector>
+
+namespace imgui_extensions
+{
+	float operator ""_deg(unsigned long long aValue)
+	{
+		return aValue * 0.0174532f;
+	}
+
+	template<class NodeType, class DrawingFunction, class ChildGetter>
+		requires
+		requires(NodeType& node, DrawingFunction&& drawing, ChildGetter&& childGetter)
+	{
+		{
+			drawing(node)
+		};
+		{
+			*std::begin(childGetter(node))
+		} -> std::convertible_to<NodeType&>;
+	}
+	ImVec2 Tree(NodeType& aNode, DrawingFunction&& aDrawingFunction, ChildGetter&& aChildGetter, ImColor aColor = ImGui::GetStyle().Colors[ImGuiCol_PlotLines], float aIndentdepth = 15.f, float aRounding = 0, float aThickness = 1)
+	{
+		ImVec2 anchor;
+		ImVec2 treeRoot;
+
+		{
+			ImVec2 start = ImGui::GetCursorScreenPos();
+			ImGui::BeginChild("node", { 0, 0 }, ImGuiChildFlags_AutoResizeY);
+
+			aDrawingFunction(aNode);
+
+			ImGui::EndChild();
+			ImVec2 end = ImGui::GetCursorScreenPos();
+
+			anchor = {
+				start.x,
+				(start.y + end.y) / 2.f
+			};
+
+			treeRoot = {
+				end.x + 5.f,
+				end.y
+			};
+		}
+
+		ImGui::Indent(aIndentdepth);
+		std::vector<ImVec2> anchors;
+		for (NodeType& child : aChildGetter(aNode))
+		{
+			ImGui::PushID(static_cast<int>(anchors.size()));
+
+			anchors.push_back(Tree(child, aDrawingFunction, aChildGetter, aColor, aIndentdepth, aRounding, aThickness));
+
+			ImGui::PopID();
+		}
+		ImGui::Unindent(aIndentdepth);
+
+		ImDrawList* drawlist = ImGui::GetWindowDrawList();
+
+		for (ImVec2& anchor : anchors)
+		{
+			ImVec2 next;
+
+			if (aRounding > 0.f)
+			{
+				float rounding = (std::min)(
+					anchor.y - treeRoot.y,
+					(std::min)(
+						anchor.x - treeRoot.x,
+						aRounding));
+
+				next = {
+					treeRoot.x,
+					anchor.y - rounding
+				};
+
+				ImVec2 arcCenter = {
+					treeRoot.x + rounding,
+					anchor.y - rounding
+				};
+
+				ImVec2 arcEnd = {
+					treeRoot.x + rounding,
+					anchor.y
+				};
+
+
+				drawlist->PathLineTo(treeRoot);
+				drawlist->PathLineTo(next);
+				drawlist->PathArcTo(arcCenter, rounding, 180_deg, 90_deg);
+				drawlist->PathLineTo(anchor);
+			}
+			else
+			{
+				next = {
+					treeRoot.x,
+					anchor.y
+				};
+
+				drawlist->PathLineTo(treeRoot);
+				drawlist->PathLineTo(next);
+				drawlist->PathLineTo(anchor);
+			}
+
+			drawlist->PathStroke(aColor, 0, aThickness);
+
+			treeRoot = next;
+		}
+
+		return anchor;
+	}
+}
